771ab2576207cde7414086a47c494f0744d2dfc5
Maksat Meredov <meredowm645@gmail.com>
2023-02-04 23:13:08 +0400

    load data

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+DATASET_FILENAME = "data.txt"
+
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(DATASET_FILENAME, 'r')
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = line.split()
+    print("  ", len(wordlist), "words loaded.")
+    return wordlist
